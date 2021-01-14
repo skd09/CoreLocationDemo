8ceb01237145027aa4258edd12303af798dbdff4
Sharvari <sharvaridivekar09@gmail.com>
2021-01-14 00:07:13 -0500

    Initial Commit

---
+++ CoreLocationDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreLocationDemo
+//
+//  Created by Work on 2021-01-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
